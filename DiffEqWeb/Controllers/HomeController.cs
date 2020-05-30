9146e0673107e921225b0de1b222d271093b0779
snxxy <re.snxxy@gmail.com>
2020-05-30 18:26:33 +0300

    Proper namespacing + dependencies

--- DiffEqWeb/Controllers/HomeController.cs
+++ DiffEqWeb/Controllers/HomeController.cs
@@ -1,12 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using DiffEqWeb.Models;
-using Microsoft.EntityFrameworkCore.Design;
 using DiffEq;
 
 namespace DiffEqWeb.Controllers
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # DiffEq
 .NET Core 3.1 
 Python 3.8 + Flask + SymPy 1.5.1
+## ToDo:
+* Fix logger crash
+* Fix scrambler
+* Refactor
